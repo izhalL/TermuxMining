bd3ffece0d60ae70adc25bb5bd0d61cdea3faab9
izhalL <60829947+izhalL@users.noreply.github.com>
2021-12-07 10:27:02 +0800

    update bot.php

--- mineltc/bot.php
+++ mineltc/bot.php
@@ -26,6 +26,26 @@ $gray = "\e[1;30m";
 echo$red."≠".$gray."=".$green."==".$green."==".$blue."==".$white."==".$yellow."==".$cyan."==".$purple."==".$gray."==".$red."==".$green."==".$blue."==".$white."==".$yellow."==".$cyan."==".$purple."==".$gray."==".$red."==".$green."==".$blue."==".$white."==".$yellow."==".$cyan."==".$purple."==".$gray."==".$green."==".$red."≠";
 }
 
+function sruput(){
+$green = "\e[1;32m";
+$blue = "\e[1;34m";
+$red = "\e[1;31m";
+$white = "\33[37;1m";
+$yellow = "\e[1;33m";
+$cyan = "\e[1;36m";
+$purple = "\e[1;35m";
+$gray = "\e[1;30m";
+
+echo$white." Sruput name      : ".$green."    MINELTC\n";
+echo$white." Wallet           : ".$blue."    Faucet".$cyan."Pay".$ye."\n";
+echo$white." Sruput ver       : ".$green."    1.0 Beta\n";
+echo$white." Created by       : ".$green."    IzhalAkbar\n";
+echo$white." Supported by     : ".$green."    Telegram \n";
+echo$white." Beta Version     : ".$green."    1.0\n";
+
+}
+
+
 function get($url,$ua){
     //header sc
 $cf = curl_init();
@@ -89,12 +109,14 @@ $get = get($url,$ua);
 $bl = explode('<span>',$get)[1];
 $ball = explode('</span> ',$bl)[0];
 
+
 strip();
-echo "
-$white Your Email      $red:     $white $email
-$white Your Ballance   $red:     $white $ball LTC
-";
+sruput();
 strip();
+echo$white." Your Email       $red:     $white $email\n" ;
+echo$white." Your Ballance    $red:     $white $ball LTC\n";
+strip();
+
 
 echo "\n\n";
 //Collect Reward
@@ -102,34 +124,40 @@ while(true){
 $url = "https://mineltc.cf/inc/data.php";
 $data = 5000000;
 $collect = post($url,$ua,$data);
-$tmr = $timer;
 
 $claimsukses = "Collected Successfully!";
 $claimfailed = "You can Collect minimum 0.00000050";
 $claimerror = "Data not transferred!";
+$tmr = $timer;
+$data = $claim;
 
 //Collect info
-
-if($data > 0){
-  if($tmr<=1)
-  echo " $claimsukses | [+]$ball \n";
-} else if($data=='Failed'){
-  echo " $claimfailed \n";
-  } else{
-    echo " $claimerror \n";
+if ($claim >=10010) {
+  echo "$claimsukses   | + $ball \n";
+} elseif ($claim==10010) {
+  echo "$claimsukses   | + $ball \n";
+} elseif ($tmr <=1) {
+  echo "$claimsukses   | + $ball \n";
+} elseif ($claim=='Failed') {
+  echo "    $claimfailed  \n";
+} else {
+  echo "    $claimerror \n";
 }
+
 
 echo "\n";
 //Timer
-for($x=$tmr;$x>-1;$x--){echo "\r \r";
-echo$blue." Updating your balance in ".$red."[".$yellow.$x.$red."] ".$green."seconds ☕🚬";
-echo "\r \r";
-sleep(1);
-  
+for ($i=$tmr;$i>-1;$i--)
+{
+  echo "\r                                   \r";
+  echo "\033[0;32m  • Tunggu Boss   "."\033[0;37m[ "."\033[0;32m".$i."\033[0;37m ]";
+if($i==60 or $i==56 or $i==50 or $i==46 or $i==40 or $i==36 or $i==30 or $i==26 or $i==20 or $i==5){echo " .";};
+if($i==58 or $i==55 or $i==49 or $i==44 or $i==39 or $i==33 or $i==28 or $i==23 or $i==10 or $i==1){echo " ..";};
+if($i==40 or $i==32 or $i==27 or $i==20 or $i==15 or $i==13 or $i==11 or $i==8 or $i==5 or $i==0){echo " ...\r";}
+   sleep(1);
 }
 
 }
-
 
 
 
