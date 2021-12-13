c0cac5a79eee8c217a208756f3da0326577756a9
izhalL <60829947+izhalL@users.noreply.github.com>
2021-12-13 18:14:30 +0800

    update cfg.php

--- mineusd/cfg.php
+++ mineusd/cfg.php
@@ -1,22 +1,9 @@
-<?
+<?php
+//------------MineUsd------------//
+$useragent = "Mozilla/5.0 (Linux; Android 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36";
 
-$useragent = "xxxxxx";
-
-$cookie = "xxxxxxx";
-
-
+$cookie = "PHPSESSID=fccahcpsbqlhao50vktbkdtikm;__.popunder=1";
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+$emdata = "acc=rizalaliakbar.1%40gmail.com&reff=&login=";
 
